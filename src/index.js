57842c8fb02133abeb73a854e62405784eb29d85
MatthiasvsGitHub <matthias@schmettow.de>
2021-04-18 16:46:44 +0200

    added add and remove function

--- src/index.js
+++ src/index.js
@@ -4,14 +4,36 @@ import Wheel from './components/wheel';
 import './styles.css';
 
 export function App () {
-   const [yacine, setYacine] = useState(false)
+  const [state, setState] = useState(true)
   
   const names = ['Clemens', 'Stavros', 'Utsavi', 'Robert', 'Said', 'Carole', 'Arno', 'Said', 'Aryan', 'Dipali', 'Ömer', 'Brian', 'Joey', 'Jasmin', 'Safi', 'Matthias'];
    
+  const addRemove = (event) => {
+    if (event.key === "Enter" && event.target.value !== "") {
+      if (names.includes(event.target.value)) {
+        for( let i = 0; i < names.length; i++){                         
+          if (names[i] === event.target.value) { 
+              names.splice(i, 1); 
+              i--;
+              alert(`${event.target.value} has been removed!`)
+              event.currentTarget.value = "";
+              setState(true)
+          }
+      }
+      }
+      else {
+        names.unshift(event.target.value)
+        alert(`${event.target.value} has been added!`)
+        event.currentTarget.value = "";
+        setState(true)
+      }
+    }
+    
+  }
     return (
       <div className="App">
         <h1>Yacine's Wheel of Doom</h1>
-        <button>Add Yacine</button>
+        <input type="text"  size="20" placeholder="Add or Remove a Name" onKeyUp={addRemove}/>
         <Wheel items={names} />
       </div>
     );
