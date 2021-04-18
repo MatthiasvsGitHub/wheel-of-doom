bbf0d4e701565f5404304ce84a3c50d9130bc2da
MatthiasvsGitHub <matthias@schmettow.de>
2021-04-18 17:15:28 +0200

    wheel now rerenders if name gets added or removed

--- src/index.js
+++ src/index.js
@@ -1,43 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Wheel from './components/wheel';
 import './styles.css';
 
-export function App () {
-  const [state, setState] = useState(true)
-  
-  const names = ['Clemens', 'Stavros', 'Utsavi', 'Robert', 'Said', 'Carole', 'Arno', 'Said', 'Aryan', 'Dipali', 'Ömer', 'Brian', 'Joey', 'Jasmin', 'Safi', 'Matthias'];
-   
+export function App() {
+  const [names, setNames] = useState(['Clemens', 'Stavros', 'Utsavi', 'Robert', 'Said', 'Carole', 'Arno', 'Said', 'Aryan', 'Dipali', 'Ömer', 'Brian', 'Joey', 'Jasmin', 'Safi', 'Matthias'])
+
   const addRemove = (event) => {
     if (event.key === "Enter" && event.target.value !== "") {
-      if (names.includes(event.target.value)) {
-        for( let i = 0; i < names.length; i++){                         
-          if (names[i] === event.target.value) { 
-              names.splice(i, 1); 
-              i--;
-              alert(`${event.target.value} has been removed!`)
-              event.currentTarget.value = "";
-              setState(true)
+      const newNames = [...names]
+      if (newNames.includes(event.target.value)) {
+        for (let i = 0; i < newNames.length; i++) {
+          if (newNames[i] === event.target.value) {
+            newNames.splice(i, 1);
+            setNames(newNames)
+            i--;
+            alert(`${event.target.value} has been removed!`)
+            event.currentTarget.value = "";
           }
-      }
+        }
       }
       else {
-        names.unshift(event.target.value)
+        newNames.unshift(event.target.value)
+        setNames(newNames)
         alert(`${event.target.value} has been added!`)
         event.currentTarget.value = "";
-        setState(true)
       }
     }
-    
+
   }
-    return (
-      <div className="App">
-        <h1>Yacine's Wheel of Doom</h1>
-        <input type="text"  size="20" placeholder="Add or Remove a Name" onKeyUp={addRemove}/>
-        <Wheel items={names} />
-      </div>
-    );
-  
+  return (
+    <div className="App">
+      <h1>Yacine's Wheel of Doom</h1>
+      <input type="text" size="20" placeholder="Add or Remove a Name" onKeyUp={addRemove} />
+      <Wheel items={names} />
+    </div>
+  );
+
 }
 /* export class App extends React.Component {
   constructor() {
